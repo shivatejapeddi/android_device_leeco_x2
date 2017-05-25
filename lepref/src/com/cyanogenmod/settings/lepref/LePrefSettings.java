91988d42bf3041d529f642f5aace47f4cdeab8d5
andr68rus <ashtmb@gmail.com>
2017-05-25 10:21:57 +0300

    LePref: add doze profiles (need framework changes)

--- lepref/src/com/cyanogenmod/settings/lepref/LePrefSettings.java
+++ lepref/src/com/cyanogenmod/settings/lepref/LePrefSettings.java
@@ -33,15 +33,18 @@ public class LePrefSettings extends PreferenceActivity implements OnPreferenceCh
 	private static final boolean DEBUG = false;
 	private static final String TAG = "LePref";
 	private static final String ENABLE_QC_KEY = "qc_setting";
-	private static final String ENABLE_HAL3_KEY = "hal3";
+//	private static final String ENABLE_HAL3_KEY = "hal3";
 	private static final String AKT_KEY = "akt";
+	private static final String DOZE_KEY = "doze";
 	private static final String QC_SYSTEM_PROPERTY = "persist.sys.le_fast_chrg_enable";
-	private static final String HAL3_SYSTEM_PROPERTY = "persist.camera.HAL3.enabled";
+//	private static final String HAL3_SYSTEM_PROPERTY = "persist.camera.HAL3.enabled";
 	private static final String AKT_SYSTEM_PROPERTY = "persist.AKT.profile";
+	private static final String DOZE_SYSTEM_PROPERTY = "persist.doze.profile";
 
 	private SwitchPreference mEnableQC;
-	private SwitchPreference mEnableHAL3;
+//	private SwitchPreference mEnableHAL3;
 	private ListPreference mAKT;
+	private ListPreference mDoze;
 
     private Context mContext;
     private SharedPreferences mPreferences;
@@ -56,13 +59,17 @@ public class LePrefSettings extends PreferenceActivity implements OnPreferenceCh
         mEnableQC.setChecked(SystemProperties.getBoolean(QC_SYSTEM_PROPERTY, false));
         mEnableQC.setOnPreferenceChangeListener(this);
                 
-        mEnableHAL3 = (SwitchPreference) findPreference(ENABLE_HAL3_KEY);
-        mEnableHAL3.setChecked(SystemProperties.getBoolean(HAL3_SYSTEM_PROPERTY, false));
-        mEnableHAL3.setOnPreferenceChangeListener(this);
+//        mEnableHAL3 = (SwitchPreference) findPreference(ENABLE_HAL3_KEY);
+//        mEnableHAL3.setChecked(SystemProperties.getBoolean(HAL3_SYSTEM_PROPERTY, false));
+//        mEnableHAL3.setOnPreferenceChangeListener(this);
         
         mAKT = (ListPreference) findPreference(AKT_KEY);
         mAKT.setValue(SystemProperties.get(AKT_SYSTEM_PROPERTY, "Stock"));
         mAKT.setOnPreferenceChangeListener(this);
+ 
+        mDoze = (ListPreference) findPreference(DOZE_KEY);
+        mDoze.setValue(SystemProperties.get(DOZE_SYSTEM_PROPERTY, "DefaultDoze.sh"));
+        mDoze.setOnPreferenceChangeListener(this);
         
         if (DEBUG) Log.d(TAG, "Initializating done");
     }
@@ -101,7 +108,29 @@ public class LePrefSettings extends PreferenceActivity implements OnPreferenceCh
 		}
 		SystemProperties.set(AKT_SYSTEM_PROPERTY, value);
     }
-   
+
+    // Set Doze
+    private void setDoze(String value) {
+		try {
+			Process su = Runtime.getRuntime().exec("su");
+			DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+			outputStream.writeBytes("sh /system/etc/lepref/doze/" + value + "\n");
+			outputStream.flush();
+			outputStream.writeBytes("exit\n");
+			outputStream.flush();
+			su.waitFor();
+		} catch(IOException e){
+			Toast toast = Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT);
+			toast.show();
+		} catch(InterruptedException e){
+			Toast toast = Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT);
+			toast.show();
+		}
+		SystemProperties.set(DOZE_SYSTEM_PROPERTY, value);
+		if (DEBUG) Log.d(TAG, "Doze mode changed to " + value);
+    }
+ 
+/* 
     // Control Camera2API
     private void setEnableHAL3(boolean value) {
 	if(value) {
@@ -111,6 +140,7 @@ public class LePrefSettings extends PreferenceActivity implements OnPreferenceCh
 	}
 	if (DEBUG) Log.d(TAG, "HAL3 setting changed");
     }
+*/
 
     @Override
     protected void onResume() {
@@ -137,17 +167,20 @@ public class LePrefSettings extends PreferenceActivity implements OnPreferenceCh
 			mEnableQC.setChecked(value);
 			setEnableQC(value);
 			return true;
-		} else if (ENABLE_HAL3_KEY.equals(key)) {
-			value = (Boolean) newValue;
-			mEnableHAL3.setChecked(value);
-			setEnableHAL3(value);
-			return true;
+//		} else  if (ENABLE_HAL3_KEY.equals(key)) {
+//			value = (Boolean) newValue;
+//			mEnableHAL3.setChecked(value);
+//			setEnableHAL3(value);
+//			return true;
 		} else if (AKT_KEY.equals(key)) {
 			strvalue = (String) newValue;
-			//mEnableHAL3.setChecked(value);
-			//setEnableHAL3(value);
 			if (DEBUG) Log.d(TAG, "AKT setting changed: " + strvalue);
 			setAKT(strvalue);
+			return true;
+		} else if (DOZE_KEY.equals(key)) {
+			strvalue = (String) newValue;
+			if (DEBUG) Log.d(TAG, "Doze setting changed: " + strvalue);
+			setDoze(strvalue);
 			return true;
 		}
           

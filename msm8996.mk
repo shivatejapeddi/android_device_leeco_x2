ccc96d7dc62c16a05a7799dce8f8b8a3ad4b539f
andr68rus <ashtmb@gmail.com>
2017-05-25 10:02:35 +0300

    Revert "Remove extra WCNSS.dat & update WCNSS ini from OP3"
    This reverts commit 8fa419582a390c77c7aed05a8c969366c7ddf6f1.

--- msm8996.mk
+++ msm8996.mk
@@ -264,7 +264,8 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf \
-    hs20-osu-client 
+    hs20-osu-client \
+    WCNSS_wlan_dictionary.dat
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
@@ -274,6 +275,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Set default USB interface

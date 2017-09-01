2db6de7e7b5c1a24f1b30af29f4d26c61b1802b9
shivatejapeddi <shivatejapeddi@gmail.com>
2017-09-01 07:33:46 +0000

    Revert "add"
    This reverts commit dbd69cd5ba5a48873a7a487b944323d43d0f0236.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,12 +6,3 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/init.power.sh
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ROOT
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-LOCAL_REQUIRED_MODULES := init.power.sh
-include $(BUILD_PREBUILT)

9ce81b98c5de3d74318645054001938b957f5678
1nfrag <sil3ntcll@gmail.com>
2017-09-06 12:53:23 +0800

    op3: Added Sustained and VR perf mode powerhints in xml Sustained performance mode: All CPUs are capped to ~1.2GHz GPU frequency is capped to 315MHz
    VR+Sustained performance mode:
    All CPUs are locked to ~1.2GHz
    GPU frequency is locked to 315MHz
    GPU BUS min_freq is raised to 790MHz
    
    VR mode:
    All CPUs are locked to ~1.44GHz
    GPU frequency is locked to 510MHz
    GPU BUS min_freq is raised to 790MHz
    
    Change-Id: I569e7861c675c4557c08d488a1def46240802714

--- device.mk
+++ device.mk
@@ -71,6 +71,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     dsi_config.xml \

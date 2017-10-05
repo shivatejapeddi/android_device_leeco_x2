ff6f08a5f8e077f354e0765c2de7613b6b6c4ab8
shivatejapeddi <shivatejapeddi@gmail.com>
2017-10-05 08:38:15 +0000

    Revert "Revert "s2: camera: import LA.BR.1.3.6_rb1.18 from caf, add cameraservice shim""
    This reverts commit a02cb740ff989be1a951cc02b41bfe7e7fb4d7cf.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# libshims_ims
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -25,6 +27,8 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+
+# libshims_camera
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := camera_shim.cpp

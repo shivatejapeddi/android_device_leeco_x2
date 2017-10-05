27a5333f6deb18561819b2fc2d2a4be7c750719d
shivatejapeddi <shivatejapeddi@gmail.com>
2017-10-05 08:51:20 +0000

    Revert "Revert "fixes""
    This reverts commit 810d43a5296e90bcdf1fd1335d5a6c6774e76a57.

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -57,12 +57,12 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../mm-image-codec/qexif \
         $(LOCAL_PATH)/../mm-image-codec/qomx_core \
         $(LOCAL_PATH)/util \
-        hardware/qcom/media-caf/msm8952/mm-core/inc
+        hardware/qcom/media-caf/msm8996/mm-core/inc
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
         frameworks/native/include/media/hardware \
-        device/leeco/s2/camera/QCamera2/HAL
+        device/leeco/x2/camera/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -87,7 +87,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display-caf/msm8952/libqservice
+        hardware/qcom/display-caf/msm8996/libqservice
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder

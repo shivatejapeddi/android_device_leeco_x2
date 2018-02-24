172509ffef0806ca1efcdf7f0d9b2e55bb342760
shivatejapeddi <shivatejapeddi@gmail.com>
2018-02-24 14:23:30 +0000

    Revert "zl1: QCamera2: Add buffer identity for video native handle."
    This reverts commit 5bbfad6a13de5288e6888c64a2cc525175f8722b.

--- camera/QCamera2/HAL/QCameraMem.h
+++ camera/QCamera2/HAL/QCameraMem.h
@@ -51,8 +51,6 @@ class QCameraMemoryPool;
 
 //OFFSET, SIZE, USAGE, TIMESTAMP, FORMAT
 #define VIDEO_METADATA_NUM_INTS          5
-//Buffer identity
-#define VIDEO_METADATA_NUM_COMMON_INTS   1
 
 enum QCameraMemType {
     QCAMERA_MEM_TYPE_DEFAULT      = 0,
@@ -233,8 +231,8 @@ public:
     int getUsage(){return mUsage;};
     int getFormat(){return mFormat;};
     int convCamtoOMXFormat(cam_format_t format);
+    native_handle_t *updateNativeHandle(uint32_t index, bool metadata = true);
     int closeNativeHandle(const void *data, bool metadata = true);
-    native_handle_t *getNativeHandle(uint32_t index, bool metadata = true);
 private:
     camera_memory_t *mMetadata[MM_CAMERA_MAX_NUM_FRAMES];
     uint8_t mMetaBufCount;
